63f81e9301b9bfd4e7e188f96935b1965e6c8fdb
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2021-05-03 08:19:12 +1000

    Update different_colours.py

--- different_colours.py
+++ different_colours.py
@@ -1,8 +1,8 @@
 from microbit import *
 import neopixel
  
-#Neopixel with 7 LEDs
-neo = neopixel.NeoPixel(pin0, 7)
+#Neopixel with 8 LEDs
+neo = neopixel.NeoPixel(pin0, 8)
  
 while True:
   neo[0] = (255, 0, 0)
@@ -12,5 +12,6 @@ while True:
   neo[4] = (0, 128, 0)
   neo[5] = (0, 0, 128)
   neo[6] = (128, 128, 0)
+  neo[7] = (0,0,0)
   neo.show()
   sleep(250)
